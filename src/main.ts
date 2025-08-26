c00b2ec65f2150c920b65346bf9cc5e5e50eea62
AnweshaSathua <119332714+AnweshaSathua@users.noreply.github.com>
2025-08-26 10:29:17 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient, withFetch } from '@angular/common/http';
- 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(withFetch()),
-    provideRouter(routes) 
-  ],
-});
+import { config } from './app/app.config.server';
+
+const bootstrap = () => bootstrapApplication(AppComponent, config);
+
+export default bootstrap;
